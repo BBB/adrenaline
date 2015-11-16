6b1fb70ab7a90c3df76129edef63a30106c60143
Ollie Relph <ollie@relph.me>
2015-11-16 12:29:05 +0000

    Whitespace

--- example/src/client/rest/adaptor.js
+++ example/src/client/rest/adaptor.js
@@ -9,47 +9,47 @@ function queryParams( obj ) {
 }
 
 function rootReducer(state, action){
-    if(action.type === UPDATE_CACHE){
-        const {type, data} = action.payload;
-        return extend(state, {
-            [type]: data
-        });
-    }
-    return state;
+  if(action.type === UPDATE_CACHE){
+    const {type, data} = action.payload;
+    return extend(state, {
+      [type]: data
+    });
+  }
+  return state;
 }
 
 export default class Adaptor extends AdrenalineAdaptor {
 
-    createCacheStore(){
-        return createStore(rootReducer, {});
-    }
+  createCacheStore(){
+    return createStore(rootReducer, {});
+  }
 
-    selectState(store, query, variables){
-        const state = store.getState();
-        const resolvedRequirements = query(variables);
-        const selectedState = reduce(resolvedRequirements, (result, params, type)=>{
-            result[type] = state[type];
-            return result;
-        }, {});
-        return Promise.resolve(selectedState);
-    }
+  selectState(store, query, variables){
+    const state = store.getState();
+    const resolvedRequirements = query(variables);
+    const selectedState = reduce(resolvedRequirements, (result, params, type)=>{
+      result[type] = state[type];
+      return result;
+    }, {});
+    return Promise.resolve(selectedState);
+  }
 
-    performQuery(store, query, variables){
-        const resolvedRequirements = query(variables)
-        const queries = map(pairs(resolvedRequirements), ([type, params])=>{
-            const queryString = Object.keys(params).reduce(function(a,k){a.push(k+'='+encodeURIComponent(params[k]));return a},[]).join('&');
-            const url = `/rest/${type}?${queryString}`;
-            return fetch(url).then((response)=>response.json()).then((json)=>{
-                this.dispatchCacheUpdate(store, {
-                    payload: {
-                        type,
-                        data: json
-                    }
-                });
-            });
-        })
-        return Promise.all(queries).then(()=>{
-            return {query, variables};
+  performQuery(store, query, variables){
+    const resolvedRequirements = query(variables)
+    const queries = map(pairs(resolvedRequirements), ([type, params])=>{
+      const queryString = Object.keys(params).reduce(function(a,k){a.push(k+'='+encodeURIComponent(params[k]));return a},[]).join('&');
+      const url = `/rest/${type}?${queryString}`;
+      return fetch(url).then((response)=>response.json()).then((json)=>{
+        this.dispatchCacheUpdate(store, {
+          payload: {
+            type,
+            data: json
+          }
         });
-    }
+      });
+    })
+    return Promise.all(queries).then(()=>{
+      return {query, variables};
+    });
+  }
 }
